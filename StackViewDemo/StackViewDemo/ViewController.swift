c1d71e3fd6c87a6079ec6576c489d003470352cd
chengaojian <chengaojian@haixun360.com>
2018-10-24 17:10:36 +0800

    新增AutoLayout布局Demo

---
+++ StackViewDemo/StackViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackViewDemo
+//
+//  Created by 三海教育 on 2018/10/24.
+//  Copyright © 2018年 chengaojian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
